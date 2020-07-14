a1ab4c1db2040c03c7144c375c08a8c684f5386e
BugenZhao <i@bugenzhao.com>
2020-07-14 12:37:13 +0800

    i386-litchi-elf-binutils: add 2.34 bottle.

--- Formula/i386-litchi-elf-binutils.rb
+++ Formula/i386-litchi-elf-binutils.rb
@@ -6,6 +6,11 @@ class I386LitchiElfBinutils < Formula
   mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.xz"
   sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
 
+  bottle do
+    root_url "https://bintray.com/bugenzhao/bottles-litchi"
+    sha256 "59efb6d5869075109f46ae81701ee9af178be834152b1b0d408f365f3f761802" => :catalina
+  end
+
   def install
     args = [
       "--prefix=#{prefix}",
