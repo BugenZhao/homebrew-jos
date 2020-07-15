69cb6e5751608f092cc8536728753055e0fbbb3d
BugenZhao <i@bugenzhao.com>
2020-07-15 15:40:20 +0800

    x86_64-litchi-elf-gdb: add 8.3.1 bottle.

--- Formula/x86_64-litchi-elf-gdb.rb
+++ Formula/x86_64-litchi-elf-gdb.rb
@@ -6,6 +6,11 @@ class X8664LitchiElfGdb < Formula
   mirror "https://ftp.gnu.org/gnu/gdb/gdb-8.3.1.tar.xz"
   sha256 "1e55b4d7cdca7b34be12f4ceae651623aa73b2fd640152313f9f66a7149757c4"
 
+  bottle do
+    root_url "https://dl.bintray.com/bugenzhao/bottles-litchi"
+    sha256 "8a3dcf3099e8197b715d10989975e998dfdd8c22426789bc660e7b6425ecc017" => :catalina
+  end
+
   def install
     args = %W[
       --prefix=#{prefix}
