b6c9b351928f80217f98abce9cc43960d066c9ad
BugenZhao <i@bugenzhao.com>
2020-07-15 15:40:20 +0800

    x86_64-litchi-elf-binutils: add 2.34 bottle.

--- Formula/x86_64-litchi-elf-binutils.rb
+++ Formula/x86_64-litchi-elf-binutils.rb
@@ -6,6 +6,11 @@ class X8664LitchiElfBinutils < Formula
   mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.xz"
   sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
 
+  bottle do
+    root_url "https://dl.bintray.com/bugenzhao/bottles-litchi"
+    sha256 "b255a6fb2772199f6dd72048b6b49207f63b38581a06e4afc9ead704354158de" => :catalina
+  end
+
   def install
     args = [
       "--prefix=#{prefix}",
